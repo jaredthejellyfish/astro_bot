86b30fd3adf03e17e1143a072c5c1a4b4a664a8e
Gerard Almenara <ger.almenara@gmai.com>
2020-01-29 01:48:47 +0100

    satellite script good

--- satellite.py
+++ satellite.py
@@ -1,51 +1,75 @@
-# API key: '79f7a1995bb5c8006f7f6ce7c542ce51'
 import requests, time, pyglet, pyowm, ephem
 
 location = 'Castelldefels, ES'
+emphem_city = 'Barcelona'
 
+#OpenWeatherMaps API key
 owm = pyowm.OWM('79f7a1995bb5c8006f7f6ce7c542ce51')
 
 def get_epoch_time():
+    #Get time
     seconds = time.time()
+    #Return rounded time in seconds
     return round(seconds)
 
-def weather(location):
+def forecasting(location):
+    #Current forecast.
     loc = owm.weather_at_place(location)
     weather = loc.get_weather()
+    #Temperature 
     temperature = weather.get_temperature('celsius')['temp']
-    return weather, temperature
+    #Humidity
+    humidity = weather.get_humidity()
+    #Wind Speed
+    wind_speed = weather.get_wind()
 
-def forecasting(location):
+    #Forecast over the next 3h.
     three_hour_forecast = owm.three_hours_forecast(location)
+    #Is it gonna rain?
     rain = three_hour_forecast.will_have_rain()
+    #Is it gonna be foggy?
     fog = three_hour_forecast.will_have_fog()
+    #Are clouds gonna roll in?
     clouds = three_hour_forecast.will_have_clouds()
-    return rain, clouds, fog
 
-def get_nit_r_day():
-    observer = ephem.city('Barcelona') # <-- put your city here
+    #Return all collected values.
+    return rain, clouds, fog, temperature, humidity, wind_speed
+
+def get_nit_r_day(emphem_city):
+    #Generate observer object with location
+    observer = ephem.city(emphem_city)
+    #Generate sun object with observer
     sun = ephem.Sun(observer)
+    #Check if sun is currently up
     sun_is_up = observer.previous_rising(sun) > observer.previous_setting(sun)
+    #Return boolean value depending on time of day
     if sun_is_up:
-        return 1
+        return True
     else:
-        return 0
+        return False
 
 def sat_vis():
+    #Pull visual light gif from Sat24
     uri = '''https://api.sat24.com/animated/SP/visual/1/width=400%20height=291'''
+    #Write pulled gif to the 'sat.gif' 
     with open('sat.gif', 'wb') as f:
         f.write(requests.get(uri).content)
 
 def sat_ir():
+    #Pull IR gif from Sat24
     uri = '''https://api.sat24.com/animated/SP/infraPolair/1/width=800%20height=582'''
+    #Write pulled gif to the 'sat.gif' 
     with open('sat.gif', 'wb') as f:
         f.write(requests.get(uri).content)
 
 def show_gif():
+    #Open 'sat.gif' file
     ag_file = 'sat.gif'
+    #Generate animation object
     animation = pyglet.resource.animation(ag_file)
     sprite = pyglet.sprite.Sprite(animation)
 
+    #Gnerate window
     win = pyglet.window.Window(width=sprite.width, height=sprite.height)
 
     green = 0, 1, 0, 1
@@ -56,18 +80,13 @@ def show_gif():
         sprite.draw()
     pyglet.app.run()
 
-def sat_img(location):
-    tod = get_nit_r_day()
+def sat_img(location,emphem_city):
+    tod = get_nit_r_day(emphem_city)
     if tod == 0:
         sat_ir()
     elif tod == 1:
         sat_vis()
 
-sat_img(location)
+print(forecasting(location))
+sat_img(location,emphem_city)
 show_gif()
-
-
-
-#print('There is going to be', forecasting(location)[0], 'rain')
-#print('There are going to be', forecasting(location)[1], 'clouds')
-#print('There is going to be', forecasting(location)[2], 'fog')
