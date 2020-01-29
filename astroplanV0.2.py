1b2e4313ff16bcb0cabbd93dbbb9c470286f64a7
Gerard Almenara <ger.almenara@gmai.com>
2020-01-29 22:00:30 +0100

    Added unknown_command handler

--- astroplanV0.2.py
+++ astroplanV0.2.py
@@ -2,34 +2,40 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from forecast import generate_link, basic_forecast
 from satellite import sat_img, sat_gif2mp4, clean
 import logging, os
-from start_text import start_text
+
+start_text = '''
+Hey!
+
+Welocme to AstroPlan, a bot designed to make you astronomy planning journey esier. 
+
+These are the current commands you can currently use: 
+•   /sat (region) (name of city) - Pulls cloud images from a satellite.
+•   /fc (name of city) - Generates a forecast for that city.
+'''
 
 emphem_city = 'Barcelona'
+
 
 updater = Updater(token='965873757:AAGDYWeqXydOHcg8PI-qMK_DSH8ojBJn2-s', use_context=True)
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 
+
 dispatcher = updater.dispatcher
+
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=start_text)
 
-def not_command(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't quite get that. Try using an actual command.")
-
 def forecast(update, context):
     usr_city = context.args
     try:
-        stringreturn = "Forecast for {} right up!".format(" ".join(usr_city).title())
+        stringreturn = "Forecast for {} coming right up!".format(" ".join(usr_city).title())
         url = generate_link(usr_city)
         context.bot.send_photo(chat_id=update.effective_chat.id, photo=url, caption=stringreturn)
     except:
         context.bot.send_message(chat_id=update.effective_chat.id, text="Please provide a valid city name.")
     
-def caps(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="got sum thicc context")
-
 def satellite(update, context):
     try:
         usr_city = " ".join(context.args[1:])
@@ -40,18 +46,27 @@ def satellite(update, context):
         context.bot.send_video(chat_id=update.effective_chat.id, video=open('sat.mp4', 'rb'), caption=caption_text)
         clean()
     except:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Please provide a valid city name.")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Please provide a valid city or region name.")
 
-caps_handler = CommandHandler('fc', forecast)
-dispatcher.add_handler(caps_handler)
+def unknown_command(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I haven't been programmed to understand that command.")
 
-caps_handler = CommandHandler('sat', satellite)
-dispatcher.add_handler(caps_handler)
+def not_command(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="My master hasn't taught me how to read normal text, please send a command.")
 
-echo_handler = MessageHandler(Filters.text, not_command)
-dispatcher.add_handler(echo_handler)
+ncm_handler = MessageHandler(Filters.text, not_command)
+dispatcher.add_handler(ncm_handler)
+
+fc_handler = CommandHandler('fc', forecast)
+dispatcher.add_handler(fc_handler)
+
+sat_handler = CommandHandler('sat', satellite)
+dispatcher.add_handler(sat_handler)
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
+
+unknown_command_handler = MessageHandler(Filters.command, unknown_command)
+dispatcher.add_handler(unknown_command_handler)
 
 updater.start_polling()
--- start_text.py
+++
@@ -1,8 +0,0 @@
-start_text = '''Hey!
-Welocme to astroplan, a bot designed to make you astronomy planning journey esier. 
-
-These are the current commands you can currently use: 
-    
-    /sat (region) (name of city) - Pulls cloud images from a satellite.
-
-    /fc (name of city) - Generates a forecast for the nearest city.'''
