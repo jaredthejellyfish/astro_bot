6035bf54c2470c4e2ad28cdcbb5777db139d2653
Gerard Almenara <ger.almenara@gmai.com>
2020-01-30 03:16:11 +0100

    Commented

--- astrometry.py
+++ astrometry.py
@@ -1,9 +1,22 @@
-import requests, json
+import requests, json, time
+
+api_key = 'ujwqfydrabjirpjm'
+
+#file_id = 'AgACAgQAAxkBAAIC3V4yKPk1gmQHHDrh344TEi0XswnfAAJqsTEbqwABkVGTkrOZwNPSv0EWthsABAEAAwIAA3kAA2dlAgABGAQ'
+#bot_token = '965873757:AAGDYWeqXydOHcg8PI-qMK_DSH8ojBJn2-s'
+
+sub_id = 0
+
+def astrometry_login():
+    login = requests.post('http://nova.astrometry.net/api/login', data={'request-json': json.dumps({"apikey": api_key})}).json()
+    session_id = login["session"]
+    return session_id
 
 def get_file_path(file_id, bot_token):
     url_file_path = 'https://api.telegram.org/bot{}/getFile?file_id={}'.format(bot_token, file_id)
     response = requests.get(url_file_path).json()['result']['file_path']
     if response == None:
+        print('we fucked up chief')
         exit
     else:
         return response
@@ -11,9 +24,59 @@ def get_file_path(file_id, bot_token):
 def generate_file_url(file_id, bot_token):
     file_path = get_file_path(file_id, bot_token)
     url = 'https://api.telegram.org/file/bot{}/{}'.format(bot_token, file_path)
+    print(url)
     return url
 
+def upload(file_id, bot_token):
+    global sub_id
+    session_id = astrometry_login()
+    file_url = generate_file_url(file_id, bot_token)
+    out = requests.post('http://nova.astrometry.net/api/url_upload', data={'request-json': json.dumps({"session": session_id, "url": file_url, "scale_units": "degwidth", "scale_lower": 0.5, "scale_upper": 1.0, "center_ra": 290, "center_dec": 11, "radius": 2.0})}).json()
+    sub_id = out["subid"]
+
+def check_status():
+    global sub_id
+    try:
+        status_check_url = 'http://nova.astrometry.net/api/submissions/'+ str(sub_id)
+        out = requests.post(status_check_url).json()
+        try:
+            is_finished = out['processing_finished']
+            return is_finished
+        except:
+            return "Yer fuked"
+    except:
+        return "Invalid"
+        exit
+
+def get_tags():
+    global sub_id
+    tags_url = 'http://nova.astrometry.net/api/jobs/'+ str(sub_id) +'/machine_tags/'
+    tags = requests.post(tags_url).json()
+    return tags['tags']
+
+def gnerate_annotated_url():
+    global sub_id
+    annotated_url = 'http://nova.astrometry.net/annotated_display/' + str(sub_id)
+    return annotated_url
+
+def generate_tags_string():
+    tag_string_lst = ['Found the following objects:\n']
+    mac_tags = list(get_tags())
+    for tag in mac_tags:
+        tag_string_lst.append('   ○ ' + tag + '\n')
+    return "".join(tag_string_lst)
 
 
-#file_id = 'BQACAgQAAxkBAAICpV4yHHmWaz0yJKaEDHyE0bwzuX3NAALSBQACIhSRUQzmb5PAjpW_GAQ'
-#bot_token = '965873757:AAGDYWeqXydOHcg8PI-qMK_DSH8ojBJn2-s'
+'''
+#upload(photo_id, bot_token)
+is_finished = check_status()
+print(is_finished)
+while str(is_finished) == "None":
+    is_finished = check_status()
+
+tags_string = generate_tags_string()
+annotated_url = gnerate_annotated_url()
+
+print(annotated_url, "\n", tags_string)
+
+'''
--- astroplanV0.2.py
+++ astroplanV0.2.py
@@ -2,9 +2,10 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import telegram.ext as telegram
 from forecast import generate_link, basic_forecast
 from satellite import sat_img, sat_gif2mp4, clean
-from astrometry import generate_file_url
-import logging, os
+from astrometry import upload, check_status
+import logging, os, requests, json, time
 
+#Initial text for when the bot is initialised with /start
 start_text = '''
 Hey!
 
@@ -15,33 +16,28 @@ These are the current commands you can currently use:
 •   /fc (name of city) - Generates a forecast for that city.
 '''
 
+#City for ephemeris, it sucks.
 emphem_city = 'Barcelona'
 
+#Solving status global var
 solving = 0
+
+#Telegram bot token 
 bot_token = '965873757:AAGDYWeqXydOHcg8PI-qMK_DSH8ojBJn2-s'
 
+#Initialization of the updater object
 updater = Updater(token=bot_token, use_context=True)
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 
-
+#Initialization of the dispatcher
 dispatcher = updater.dispatcher
 
-def removeTelegramMessageHandler(self, handler):
-        """
-        De-registers a message handler.
-        Args:
-            handler (any):
-        """
-
-        if handler in self.telegram_message_handlers:
-            self.telegram_message_handlers.remove(handler)
-
-
-
+#Bot initialized with /start
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=start_text)
 
+#Generation of forecast url and submission to user.
 def forecast(update, context):
     usr_city = context.args
     try:
@@ -50,7 +46,8 @@ def forecast(update, context):
         context.bot.send_photo(chat_id=update.effective_chat.id, photo=url, caption=stringreturn)
     except:
         context.bot.send_message(chat_id=update.effective_chat.id, text="Please provide a valid city name.")
-    
+
+#Generation and upload of Satellite images for user.
 def satellite(update, context):
     try:
         usr_city = " ".join(context.args[1:])
@@ -63,60 +60,69 @@ def satellite(update, context):
     except:
         context.bot.send_message(chat_id=update.effective_chat.id, text="Please provide a valid city or region name.")
 
+#Unknown command handling.
 def unknown_command(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I haven't been programmed to understand that command.")
 
+#Normal text (no "/"") detection and handling.
 def not_command(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="My master hasn't taught me how to read normal text, please send a command.")
 
+#Astrometry upload, solving, and result fetching.
 def platesolve_image(update, context):
     global solving
     if solving == 1:
         try:
             file_id = str(update.message.document.file_id)
-            generate_file_url(file_id, bot_token)
+            upload(file_id, bot_token)
+            check_status()
         except:
             pass
+
         try:
             photo_id = str(update.message.photo[-1].file_id)
-            generate_file_url(photo_id, bot_token)
-        except:
-            context.bot.send_message(chat_id=update.effective_chat.id, text="Your file is too large :(")
+            upload(photo_id, bot_token)
+            check_status()
+            print(photo_id)
             
+        except :
+            context.bot.send_message(chat_id=update.effective_chat.id, text="Your file is too large :(")
     else:
         context.bot.send_message(chat_id=update.effective_chat.id, text="Send a command before uploading a picture")
     solving = 0
 
-
+#Enabler for image detection and upload.
 def platesolve(update, context):
     global solving
     solving = 1
     
-
+#Handler for forecast.
 fc_handler = CommandHandler('fc', forecast)
 dispatcher.add_handler(fc_handler)
 
+#Handler for satellite.
 sat_handler = CommandHandler('sat', satellite)
 dispatcher.add_handler(sat_handler)
 
+#Handler dor start.
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
+#Handler for platesolver.
 solve_handler = CommandHandler('solve', platesolve)
 dispatcher.add_handler(solve_handler)
 
+#Handler for image solving.
 platesolve_image_handler = MessageHandler(Filters.photo | Filters.document, platesolve_image)
 dispatcher.add_handler(platesolve_image_handler, group=1)
 
+#Handler for normal text (not prefaced by "/")
 not_command_handler = MessageHandler(Filters.text, not_command)
 dispatcher.add_handler(not_command_handler)
 
+#Handler for unknown commands.
 unknown_command_handler = MessageHandler(Filters.command, unknown_command)
 dispatcher.add_handler(unknown_command_handler)
 
-
+#Start the updater.
 updater.start_polling()
-
-
-#file_id = str(update.message.document.file_id)
-        #print(generate_file_url(file_id, bot_token))
