3fb88b28550834c96049647010171e945b69645e
Gerard Almenara <ger.almenara@gmai.com>
2020-02-03 20:39:07 +0100

    -

--- tests.py
+++
@@ -1,8 +0,0 @@
-
-
-def generate_tags_string(job_id):
-    tag_string_lst = get_ra_dec_tags(job_id)
-    mac_tags = list(get_tags_objects(job_id))
-    formatted_mac_tag_string = ['   ○ ' + tag + '\n' for tag in mac_tags]
-    tag_string_lst.append(formatted_mac_tag_string)
-    return "".join(tag_string_lst)
