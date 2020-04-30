218d2ac03a9ffb99dfbc5fa2fe5a92481de7f5e0
jarredthejellyfish <ger.almenara@gmail.com>
2020-04-30 20:09:06 +0200

    satellite.py updated

---
+++ dev.py
@@ -0,0 +1,5 @@
+from pygeocoder import Geocoder
+
+gc = Geocoder('AIzaSyAeegl5a8C2iHR4CUvcWIE1OOSfCHEnqZ0')
+results = gc.reverse_geocode(41.28610, 1.98241)
+
--- satellite.py
+++ satellite.py
@@ -1,6 +1,9 @@
 import requests
 from urllib.request import urlopen
 import json
+from pygeocoder import Geocoder
+
+gc = Geocoder('AIzaSyAeegl5a8C2iHR4CUvcWIE1OOSfCHEnqZ0')
 
 country_codes = {'Germany':'DE', 
                  'Spain':'SP', 
@@ -19,27 +22,14 @@ country_codes = {'Germany':'DE',
                  'United Kingdom':'UK'
                 }
 
-def getplace(lat, lon):
-    url = "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyAeegl5a8C2iHR4CUvcWIE1OOSfCHEnqZ0&"
-    url += "latlng=%s,%s&sensor=false" % (lat, lon)
-    v = urlopen(url).read()
-    j = json.loads(v)
-    components = j['results'][0]['address_components']
-    country = town = None
-    for c in components:
-        if "country" in c['types']:
-            country = c['long_name']
-        if "postal_town" in c['types']:
-            town = c['long_name']
-    return town, country
-
-def get_country_code(country):
+def get_country_code(lat, lon):
+    results = gc.reverse_geocode(lat, lon)
+    country = results.country
     if country and country in country_codes.keys():
         return country_codes[country]
 
 def get_sat(lat, lon, chat_id='test_satellite_image'):
-    country = getplace(lat, lon)[1]
-    country_code = get_country_code(country)
+    country_code = get_country_code(lat, lon)
     if country_code:
         uri = 'https://api.sat24.com/animated/{}/visual/1/Romance%20Standard%20Time/2964777'.format(country_code)
         with open('{}.gif'.format(chat_id), 'wb') as f:
